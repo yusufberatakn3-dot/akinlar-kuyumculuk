2786846bff19b5164ebd2e09eaf51becde830881
yusufberatakn3-dot <yusufberatakn3@gmail.com>
2026-06-10 15:18:19 +0300

    Update prices.js

--- api/prices.js
+++ api/prices.js
@@ -3,11 +3,13 @@ export default async function handler(req, res) {
     const response = await fetch("https://finans.truncgil.com/today.json");
 
     const text = await response.text();
+    const son = text.lastIndexOf("}");
+const temiz = son > -1 ? text.slice(0, son + 1) : text;
 
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Content-Type", "application/json");
 
-    res.status(200).send(text);
+    res.status(200).send(temiz);
 
   } catch (error) {
     res.status(500).json({
